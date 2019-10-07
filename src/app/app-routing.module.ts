5ef45f2cde0153d3b5ab3b8ea5deb55d7cbba944
wylliansales <wylliansalles@gmail.com>
2019-10-07 00:22:11 -0300

    Iniciando desenvolvimento diario-saude

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,18 @@ import {AdduserComponent} from "~/app/perfil/adduser/adduser.component";
 import {HomeComponent} from "~/app/home/home.component";
 import {PutuserComponent} from "~/app/perfil/putuser/putuser.component";
 import {VisualizacoesComponent} from "~/app/visualizacoes/visualizacoes.component";
+import {DiarioSaudeComponent} from "~/app/diario-saude/diario-saude.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full"},
     { path: "index", component: IndexComponent},
     { path: "login", component: LoginComponent },
     { path: "adduser", component: AdduserComponent },
-    { path: "home", component: HomeComponent, canActivate: [AppAuthGuard]},
+    //{ path: "home", component: HomeComponent, canActivate: [AppAuthGuard]},
+    { path: "home", component: HomeComponent},
     { path: "putuser", component: PutuserComponent},
     { path: "visualizacao", component: VisualizacoesComponent},
+    { path: "diario", component: DiarioSaudeComponent },
 ];
 
 @NgModule({
