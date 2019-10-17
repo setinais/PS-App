f827b06a6ac35254f0b4542d789d251060787043
Vinnicyus Carvalho Gonçalves <vynny.cg@gmail.com>
2019-10-17 15:49:16 -0300

    detail

--- src/app/visualizacoes-detail/visualizacoes-detail.component.ts
+++ src/app/visualizacoes-detail/visualizacoes-detail.component.ts
@@ -11,28 +11,29 @@ import * as TNSPhone from "nativescript-phone";
 })
 export class VisualizacoesDetailComponent implements OnInit {
 
-    private dados: VisualizacaoModel
-// {
-//         nome: 'Hospital Modelo',
-//         localizacao: {latitude: -10.1753214, longitude: -48.8849307},
-//         endereco: 'Rua Tapajós, Quadra 24, Lote1, 260 ',
-//         bairro: 'Centro',
-//         telefone: '(63) 3602-1188',
-//         imagem: '~/imagens/setembroamarelo.jpg',
-//         descricao: 'Posto Avançado de saude, destinado ao atendimento rapido de cidadões que apresentam sintomas de doenças de possivel tratamento na unidade atual. Se identificado a incapacidade da UBS, o paciente sera encaminhado ao Hospital mais proximo.',
-//         servicos: [
-//             { s: 'ESTRATEGIA DE SAUDE DA FAMILIA', c: 'SAUDE BUCAL MI' },
-//             { s: 'ESTRATEGIA DE SAUDE DA FAMILIA', c: 'SAUDE DA FAMILIA' },
-//             { s: 'SERVICO DE ATENCAO A SAUDE DO TRABALHADOR', c: 'ATENDIMENTO ACOMPANHAMENTO EM SAUDE DO TRABALHADOR' },
-//             { s: 'SERVICO DE ATENCAO AO PACIENTE COM TUBERCULOSE', c: 'DIAGNOSTICO E TRATAMENTO' },
-//         ],
-//         created_at: 'string'
-//     }
+    private dados: VisualizacaoModel =
+{
+        nome: 'Hospital Modelo',
+        localizacao: {latitude: -10.1753214, longitude: -48.8849307},
+        endereco: 'Rua Tapajós, Quadra 24, Lote1, 260 ',
+        bairro: 'Centro',
+        telefone: '(63) 3602-1188',
+        imagem: '~/imagens/setembroamarelo.jpg',
+        descricao: 'Posto Avançado de saude, destinado ao atendimento rapido de cidadões que apresentam sintomas de doenças de possivel tratamento na unidade atual. Se identificado a incapacidade da UBS, o paciente sera encaminhado ao Hospital mais proximo.',
+        servicos: [
+            { s: 'ESTRATEGIA DE SAUDE DA FAMILIA', c: 'SAUDE BUCAL MI' },
+            { s: 'ESTRATEGIA DE SAUDE DA FAMILIA', c: 'SAUDE DA FAMILIA' },
+            { s: 'SERVICO DE ATENCAO A SAUDE DO TRABALHADOR', c: 'ATENDIMENTO ACOMPANHAMENTO EM SAUDE DO TRABALHADOR' },
+            { s: 'SERVICO DE ATENCAO AO PACIENTE COM TUBERCULOSE', c: 'DIAGNOSTICO E TRATAMENTO' },
+        ],
+        created_at: 'string'
+    }
 
     private length_dados: string
 
   constructor(private route: ActivatedRoute) {
-      this.dados = this.route.queryParams['_value']
+      // this.dados = this.route.queryParams['_value']
+      console.log(this.dados)
       this.lenghtDados();
   }
 
